6043112cafe7a5a12e27a062486ac10c2759820f
Kenneth Treadaway <121038701+CyberKenneth@users.noreply.github.com>
2024-01-24 14:26:48 -0600

    Create Greeter.java
    hello world like program that says hi to Steve part of lab 1

---
+++ workspace/project1/src/lab1/Greeter.java
@@ -0,0 +1,19 @@
+/**
+Kenneth J. Treadaway
+1/24/2024 14:25
+simple hello world like program.
+ * 
+ */
+package lab1;
+
+/**
+ * 
+ */
+public class Greeter 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello, Steve");
+	}
+}
+
